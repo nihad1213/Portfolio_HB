1d4d7e04c2495250b17cb2271f1878f0c417072d
nihad1213 <nihad.nemetli@gmail.com>
2024-10-11 02:31:24 +0400

    subcribers can be added from index.html part

--- models/Subscriber.py
+++ models/Subscriber.py
@@ -3,13 +3,13 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.BaseModel import BaseModel
 
-class Subscriber(BaseModel):
+class Subscribers(BaseModel):
     __tablename__ = 'subscribers'
+    
+    email = Column(String(120), nullable=False, unique=True)
 
-    user_id = Column(String(36), ForeignKey('users.id'), nullable=False)  # Foreign key to User
-
-    def __init__(self, user_id):
-        self.user_id = user_id
+    def __init__(self, email):
+        self.email = email
 
     def __repr__(self):
-        return f"<Subscriber(user_id='{self.user_id}')>"
+        return f"<Subscriber {self.email}>"
